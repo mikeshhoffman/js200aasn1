4de2acbcf07c6c36cc2164655021972aebf23031
Michael S. Hoffman <mikeshoffman@gmail.com>
2019-01-23 18:30:32 -0800

    fix addressblock parsing, clean up js source files

--- addressParse/script.js
+++ addressParse/script.js
@@ -2,8 +2,8 @@
 let firstName = "Michael";
 let lastName = "Hoffman";
 let streetAddress = "4672 Eastern Av. N";
-let city = "Seattle";
-let state = "WA";
+let city = "New Seattle";
+let state = "Old Washington";
 let zipCode = "98103";
 
 // Address block format:
@@ -12,60 +12,41 @@ let zipCode = "98103";
 // city, state zip
 let addressBlock = firstName + " " + lastName + "\n" + streetAddress + "\n" + city + ", " + state + " " + zipCode;
 
+console.log("addressBlock:\n", addressBlock);
+
 
 // 2. Given an address string in format:
 // firstName lastName  (no space in either)
 // streetAddress
-// city, state zip (city and state can have a space)
+// city, state zip  (city and state can have a space)
 // eg: 
 // let myAddr = 'Matt Schneider\n111 2nd Ave. W\nSeattle, WA 98199'
 
-// get all the string up to the first space
-// extract that string into vars.
-// use any of: indexOf(), slice(), substring(), 
-// "split is much easier"
+// Get all the string up to the first space.
+// Extract that string into variables.
+// Use any of: indexOf(), slice(), substring(), split().
+// Split is much easier.
+
+console.log("Extracting the fields from the address block: ");
 
 let addrParts = addressBlock.split('\n');
 let fullName = addrParts[0];
-console.log("fullName: ", fullName);
 let streetAddr = addrParts[1];
-console.log("streetAddr: ", streetAddr);
 let cityStateZip = addrParts[2];
-console.log("cityStateZip: ", cityStateZip);
 
 let firstNm = fullName.substring(0, fullName.indexOf(' '));
 console.log("firstNm: ", firstNm);
 let lastNm = fullName.substring(fullName.indexOf(' '), fullName.length);
 console.log("lastNm: ", lastNm);
 
-
-let cityResult = cityStateZip.substring(0, cityStateZip.indexOf(' '));
+let cityResult = cityStateZip.substring(0, cityStateZip.indexOf(','));
 console.log("cityResult: ", cityResult);
-let stateResult = cityStateZip.substring(cityStateZip.indexOf(' '), cityStateZip.length);
+
+
+let stateZip = cityStateZip.substring(cityStateZip.indexOf(',') + 2, cityStateZip.length);
+
+let stateResult = stateZip.substring(0, stateZip.lastIndexOf(' '));
 console.log("stateResult: ", stateResult);
-let zipResult = cityStateZip.substring(cityStateZip.indexOf(' '), cityStateZip.length);
+
+let zipResult = stateZip.substring(stateZip.lastIndexOf(' '), stateZip.length);
 console.log("zipResult: ", zipResult);
-
-// Matt's solution:
-//"Matt".indexOf('M');
-
-// his nonarray approach:
-//let address = 'Matt Schneider\n111 2nd Ave. W\nSeattle, WA 98199';
-//const space1Index = address.index(' ');
-//let myFirstName = address.slice(0, space1Index);
-//const endOfLineIndex = address.indexOf('\n');
-//let myLastName = address.slice(space1Index + 1, endOfLineIndex);
-
-
-// from Zoom Group Chat:
-//var fullAddress = `${firstName} ${lastName}
-//  ${streetAddress}
-//  ${city}, ${state} ${zipCode}`;
-//
-// fullName = `${firstName} ${lastName}`; // ` backtick
-//let mailingAddress = `${fullName}\n${address}`;
-//console.log(mailingAddress);
-
-
-// 3. find the middle date: 
-
--- pizza/script.js
+++ pizza/script.js
@@ -1,4 +1,4 @@
-// 1. Using the Math object, put together a code snippet that allows you to draw a random card with a value between 1 and 13 (assume ace is 1, jack is 11...).
+// 1. Using the Math object, put together a code snippet that allows you to draw a random card with a value between 1 and 13 (assume ace is 1, jack is 11).
 var card = Math.floor(Math.random() * (13)) + 1;
 console.log("card: ", card);
 
@@ -6,16 +6,18 @@ console.log("card: ", card);
 // 2. Draw 3 cards and use Math to determine the highest card.
 var card1 = Math.floor(Math.random() * 13) + 1;
 console.log("card1: ", card1);
+
 var card2 = Math.floor(Math.random() * 13) + 1;
 console.log("card2: ", card2);
+
 var card3 = Math.floor(Math.random() * 13) + 1;
 console.log("card3 ", card3);
+
 var cardMax = Math.max(card1, card2, card3);
 console.log("cardMax: ", cardMax);
 
 
 //3. Pagliacci charges $16.99 for a 13” pizza and $19.99 for a 17” pizza. What is the surface area for each of these pizzas?
-
 var diam1 = 13;
 var surfaceArea1 = Math.PI * ((diam1 / 2.0) ** 2);
 console.log("surfaceArea of 13-inch pizza: ", surfaceArea1);
